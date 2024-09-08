ee2a06ec32c6464e0b7b9a3cd9e323ca543f79b9
ameyakarve <ameyakarve@users.noreply.github.com>
2024-09-08 20:12:04 +0530

    added styling for control panel

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -12,22 +12,33 @@ function ControlPanel() {
 
   return (
     <div className="control-panel">
-      <div className="values-section">
-        <label>Values:</label>
-        <div className="chips">
-          {chips.map((chip) => (
-            <button
-              key={chip}
-              className={`chip ${selectedChip === chip ? 'selected' : ''}`}
-              onClick={() => handleChipClick(chip)}
-            >
-              {chip}
-            </button>
-          ))}
+      <div className="panel-content">
+        <div className="chips-container">
+          <span className="chips-label">VALUES</span>
+          <div className="chips">
+            {chips.map((chip) => (
+              <button
+                key={chip}
+                className={`chip ${selectedChip === chip ? 'selected' : ''}`}
+                onClick={() => handleChipClick(chip)}
+              >
+                {chip}
+              </button>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="icons-section">
-        {/* Add your icon buttons here */}
+        <div className="icons">
+          <button className="icon-button" title="Sort">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M3 18h6v-2H3v2zM3 6v2h18V6H3zm0 7h12v-2H3v2z"/>
+            </svg>
+          </button>
+          <button className="icon-button" title="Filter">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/>
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
